24cee37bf71b854e92e29bbe0854bb2f2224efec
Maru <marujoprofissional@gmail.com>
2026-05-14 19:55:33 -0300

    feat: Creating login model

--- WebProject1/Models/LoginViewModel.cs
+++ WebProject1/Models/LoginViewModel.cs
@@ -2,5 +2,11 @@
 {
     public class LoginViewModel
     {
+        //Encapsulamento - modificadores get; set;
+        public int UserID { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string UserEmail { get; set; } = string.Empty;
+        public string UserPassword { get; set; } = string.Empty;
+        public string UserLevel { get; set; } = "user";
     }
 }
