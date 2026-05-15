2d38fdf98e36f9ed7fc72193b3c6771b432ed8d4
Maru <marujoprofissional@gmail.com>
2026-05-14 21:21:54 -0300

    feat: Interface of User Repositorie done

---
+++ WebProject1/Interfaces/IUserRepositorie.cs
@@ -0,0 +1,16 @@
+﻿using WebProject1.Models;
+
+namespace WebProject1.Interfaces
+{
+    public interface IUserRepositorie
+    {
+        /*
+         Interface funciona como um contrato,
+        define oque uma classe deve fazer
+        e quais metodos e propriedades terá,
+        mas não diz como deve fazer.
+         */
+
+        LoginViewModel Validate(string email, string password);
+    }
+}
